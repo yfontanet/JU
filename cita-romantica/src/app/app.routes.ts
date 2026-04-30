cf668f8d42b5ae684053c0049a71e2f4c233bbf7
yfontanet <yaiza.fontanet222@comunidadunir.net>
2026-04-30 11:04:15 +0200

    Auto-generated commit

--- cita-romantica/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
-import { Pedido } from './pages/pedido/pedido';
-
-export const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'pedido', component: Pedido },
-];
